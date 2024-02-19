ea6b89f59993a8c8189de27f64feca6a84278d99
Aeroxee <fajhri@aZ4ziL>
2024-02-19 18:38:02 +0700

    get value from url query

---
+++ handlers/queries.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func getQueryString(r *http.Request, key, defaultValue string) string {
+	result := r.URL.Query().Get(key)
+	if result == "" {
+		return defaultValue
+	}
+
+	return result
+}
+
+func getQueryInt(r *http.Request, key string, defaultValue int) int {
+	result := r.URL.Query().Get(key)
+	if result == "" {
+		return defaultValue
+	}
+
+	resultInt, err := strconv.Atoi(result)
+	if err != nil {
+		return defaultValue
+	}
+
+	return resultInt
+}
+
+func getQueryBool(r *http.Request, key string, defaultValue bool) bool {
+	result := r.URL.Query().Get(key)
+	if result == "" {
+		return defaultValue
+	}
+
+	resultBool, err := strconv.ParseBool(result)
+	if err != nil {
+		return defaultValue
+	}
+
+	return resultBool
+}
