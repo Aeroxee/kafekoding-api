e1a59d74addcc3f43e979e9ce9fcde81406096bd
Aeroxee <fajhri@aZ4ziL>
2024-02-19 19:28:11 +0700

    model for article comment

--- models/articlecomment.go
+++ models/articlecomment.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// ArticleComment
+type ArticleComment struct {
+	ID        int            `gorm:"primaryKey" json:"id"`
+	ArticleID int            `json:"article_id"`
+	UserID    int            `json:"user_id"`
+	Text      string         `gorm:"type:text" json:"text"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	CreatedAt time.Time      `json:"created_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
